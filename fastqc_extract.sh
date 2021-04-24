7222849bb60e79d5b15ce3da5a4ff5d95511980d
Dwight Newton <38666846+DwightNewton@users.noreply.github.com>
2021-04-24 13:30:23 -0400

    Delete fastqc_extract.sh

--- fastqc_extract.sh
+++
@@ -1,9 +0,0 @@
-#First, cd to fastqc output location and run # unzip "*.zip"
-SRC_DIR="/external/mgmt1/scratch01/tmpdata02/dnewton/PITT_tetrad_SCT_RNAseq/fastq/pool_1/Samples/fastqc/"
-out_dir="/external/mgmt1/scratch01/tmpdata02/dnewton/PITT_tetrad_SCT_RNAseq/fastq/pool_1/Samples/fastqc/fqc_res/"
-cd $SRC_DIR
-for file in *.zip;
-do
-Base_Name=${file%.zip}
-echo "cp $SRC_DIR/${Base_Name}/summary.txt $out_dir/$Base_Name.txt"
-done
